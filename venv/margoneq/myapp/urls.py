31a8e55bcb515f9d33df8872a06931950f3af5ab
ernestokseniuk <66172286+ernestokseniuk@users.noreply.github.com>
2020-05-30 21:03:36 +0200

    Update urls.py

--- venv/margoneq/myapp/urls.py
+++ venv/margoneq/myapp/urls.py
@@ -1,14 +1,14 @@
-from django.conf.urls import url
+from django.urls import path
 from myapp import views
 
 
 #TEMPLATE TAGGING
 app_name= 'myapp'
 urlpatterns = [
-    url(r'register/$', views.register, name='register'),
-    url(r'user_login/$',views.user_login, name="user_login"),
-    url(r'additem/$',views.addItem, name="addItem"),
-    url(r'^(?P<itemid>[0-9]+)/$',views.itemView,name="itemdet"),
-    url(r'users/(?P<userid>[0-9]+)/$', views.userView, name="userdet"),
-    url(r'^(?P<itemid>[0-9]+)/a/a/a',views.itemDel, name = "comDel"),
+    path('register/', views.register, name='register'),
+    path('user_login/',views.user_login, name="user_login"),
+    path('additem/',views.addItem, name="addItem"),
+    path('int:itemid>/',views.itemView,name="itemdet"),
+    path('users/<int:userid>/', views.userView, name="userdet"),
+    path('<int:itemid>/a/a',views.itemDel, name = "comDel"),
 ]
