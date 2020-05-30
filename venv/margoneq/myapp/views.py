a2010af0c0bae8d051116bc7cdd3ababf3de6ddb
ernestokseniuk <66172286+ernestokseniuk@users.noreply.github.com>
2020-05-30 21:04:00 +0200

    Update views.py

--- venv/margoneq/myapp/views.py
+++ venv/margoneq/myapp/views.py
@@ -6,7 +6,7 @@ from . import models
 from . import codegen
 from . import forms
 from myapp.forms import UserForm, UserProfileInfoForm, AddItemForm, AddCommentForm
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.decorators import  login_required
 from django.http import HttpResponseRedirect, HttpResponse
